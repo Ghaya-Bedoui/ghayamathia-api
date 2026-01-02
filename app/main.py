1357c56eea62af6e22cead3cd3cf9dc74c5d50ca
Ghaya-Bedoui <mounabedoui9@gmail.com>
2026-01-02 21:01:55 +0100

    fix: import routers from app.api.routes

--- app/main.py
+++ app/main.py
@@ -3,7 +3,8 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 from app.core.config import settings
-from app.api import auth, courses
+from app.api.routes import auth, courses
+
 
 app = FastAPI(
     title=settings.APP_NAME,
